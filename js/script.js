dba0186b1e3440f402b531efe009b2a471c823dd
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-06-30 03:56:30 +0300

    ааа

--- js/script.js
+++ js/script.js
@@ -1,108 +1,72 @@
-// *
-//  * Типів транзацкій всього два.
-//  * Можна покласти або зняти гроші з рахунку.
-//  */
 const Transaction = {
   DEPOSIT: 'deposit',
-  WITHDRAW: 'withdraw', 
+  WITHDRAW: 'withdraw',
 };
 
-/*
- * Кожна транзакція - це об'єкт з властивостями: id, type і amount
- */
+
+const transactions = [];
+let transactionId = 0;
+
 const account = {
-    id: 1,
-  // Поточний баланс рахунку
   balance: 0,
 
-  // Історія транзакцій
 
-  transactions: [],
-  /*
-   * Метод створює і повертає об'єкт транзакції.
-   * Приймає суму і тип транзакції.
-   */
 
-  createTransaction(amount, type) {const newTransaction = {
-   amount: amount,
-   type: type,
-   id: this.id,
-  }
-  this.id += 1
-  return newTransaction
-},
-  /*
-   * Метод відповідає за додавання суми до балансу.
-   * Приймає суму танзакції.
-   * Викликає createTransaction для створення об'єкта транзакції
-   * після чого додає його в історію транзакцій
-   */
+  createTransaction(amount, type) {
+    return {
+      id: ++transactionId,
+      type,
+      amount,
+    };
+  },
+
   deposit(amount) {
-    this.balance += amount
-    const transaction = this.createTransaction(amount, "deposit")
-    this.transactions.push(transaction)
+    this.balance += amount;
+    const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
+    transactions.push(transaction);
   },
 
-  /*
-   * Метод відповідає за зняття суми з балансу.
-   * Приймає суму танзакції.
-   * Викликає createTransaction для створення об'єкта транзакції
-   * після чого додає його в історію транзакцій.
-   *
-   * Якщо amount більше, ніж поточний баланс, виводь повідомлення
-   * про те, що зняття такої суми не можливо, недостатньо коштів.
-   */
   withdraw(amount) {
-    
-
-if (amount < this.withdraw) {
-this.balance -= amount
-    const transaction = this.createTransaction(amount, "withdraw")
-    this.transactions.push(transaction)
-} else {alert('не достатньо коштів')}
-  },
-
-  /*
-   * Метод повертає поточний баланс
-   */
-  getBalance() {
-    return this.balance
-  },
-  /*
-   * Метод шукає і повертає об'єкт транзакції по id
-   */
-  getTransactionDetails(id) {
-    for (const element of this.transactions) {
-        // console.log(element);
-        if (element.id ===id) {
-            return element;
-        } else {
-            return null
-        }
+    if (amount > this.balance) {
+      console.log('Недостатньо коштів для зняття!');
+      return;
     }
-    
+    this.balance -= amount;
+    const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+    transactions.push(transaction);
   },
 
-  /*
+  getBalance() {
+    return this.balance;
+  },
 
-   * Метод повертає кількість коштів
-   * певного типу транзакції з усієї історії транзакцій
-   */
-  getTransactionTotal(type) {},
+  getTransactionDetails(id) {
+    return transactions.find(({ id: transId }) => transId === id);
+  },
+
+  getTransactionTotal(type) {
+    return transactions.reduce((total, { type: t, amount }) => {
+      return t === type ? total + amount : total;
+    }, 0);
+  },
+
+  get transactionsList() {
+    return transactions;
+  },
 };
 
-account.deposit(123)
-account.deposit(123)
-account.deposit(123)
-account.deposit(123)
-account.withdraw(111)
-account.withdraw(111)
-account.withdraw(111)
-console.log(account.getBalance());
-console.log(account.getTransactionDetails());
-console.log(account.getTransactionTotal());
 
+account.deposit(1000);
+account.withdraw(300);
+account.deposit(200);
+account.withdraw(800); 
 
-console.log(account);
+console.log('Поточний баланс:', account.getBalance());
 
+console.log('Транзакція з ID 2:', account.getTransactionDetails(2));
 
+console.log('Сума депозитів:', account.getTransactionTotal(Transaction.DEPOSIT));
+console.log('Сума зняття:', account.getTransactionTotal(Transaction.WITHDRAW));
+
+console.log('Уся історія транзакцій:');
+console.table(account.transactionsList);
